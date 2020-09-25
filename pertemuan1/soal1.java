d3a908dc98a1461d7c2caf1e9da864eac4e8dbc8
MuhammadAnggaSaputra <Manggas703@gmail.com>
2020-09-26 01:52:13 +0700

    array

---
+++ pertemuan1/soal1.java
@@ -0,0 +1,27 @@
+//Muhammad Angga Saputra
+//193040070
+
+public class soal1
+{
+	public void run()
+	{
+		//deklarasi array integer
+		int[] arr;
+		arr = new int[5];
+		
+		//mengisi elemen array
+		arr[0] = 5;
+		arr[1] = 10;
+		arr[2] = 15;
+		arr[3] = 20;
+		arr[4] = 25;
+		
+		//akses elemen array untuk ke layar
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		
+	}
+}
---
+++ pertemuan1/utama1.java
@@ -0,0 +1,11 @@
+//muhammad Angga Saputra
+//193040070
+
+public class utama1
+{
+	public static void main(String[] args)
+	{
+		soal1 program = new soal1();
+		program.run();
+	}
+}
