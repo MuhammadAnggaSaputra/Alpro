d752fa478b07b382a0971e8d5a875d36ffc99fa8
MuhammadAnggaSaputra <Manggas703@gmail.com>
2020-09-26 00:49:39 +0700

    File awal Alpro

---
+++ pertemuan1/soal2.java
@@ -0,0 +1,25 @@
+//Muhammad Angga Saputra
+//193040070
+
+public class soal2
+{
+	public void run()
+	{
+		//deklarasi array string
+		String[] arr;
+		arr = new String[4];
+		
+		//mengisi elemen array
+		arr[0] = "Ibu";
+		arr[1] = "pergi";
+		arr[2] = "ke";
+		arr[3] = "pasar.";
+		
+		//Akses elemen array untuk ke layar
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		
+	}
+}
---
+++ pertemuan1/utama2.java
@@ -0,0 +1,11 @@
+//muhammad Angga Saputra
+//193040070
+
+public class utama2
+{
+	public static void main(String[] args)
+	{
+		soal2 program = new soal2();
+		program.run();
+	}
+}
