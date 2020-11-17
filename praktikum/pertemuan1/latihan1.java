b9ed83d26e3ff5d32027af5554925d040b458183
MuhammadAnggaSaputra <Manggas703@gmail.com>
2020-11-17 08:58:22 +0700

    menambahkan file pertemuan 1

---
+++ praktikum/pertemuan1/latihan1.java
@@ -0,0 +1,13 @@
+package pertemuan1;
+
+import java.util.Scanner;
+
+public class latihan1 {
+	public static void main(String[] args) {
+		int [] arrNilai = {54, 45, 25, 30, 44};
+		
+		for (int indeks = 0; indeks < arrNilai.length; indeks++) {
+			System.out.println(arrNilai[indeks]);
+		}
+	}
+}
